85a19ce62301eda8dcc9aa52ce6afcf4817ecbe4
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-04-27 14:54:26 +0430

    state & setstate in class component (clock) new declaration state

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -3,10 +3,10 @@ import { Component } from "react";
 class Clock extends Component{
   constructor(props){
     super(props);
-    this.state = {
-      time: new Date()
-    }
     setInterval(() => this.tick(),1000);
+  }
+  state = {
+    time: new Date()
   }
   render(){
     return(
