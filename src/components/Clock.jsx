6873ea71eaf66d61b2fc70600a2d0f3089be7f41
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-04-27 19:15:56 +0430

    preventDefault & stopPropagation in CC

--- src/components/Clock.jsx
+++ src/components/Clock.jsx
@@ -1,6 +1,10 @@
 import { Component } from "react";
 
 class Clock extends Component {
+  constructor(props){
+    super(props);
+    this.handleClick = this.handleClick.bind(this); //this is 1 solution for bind (This)
+  }
   state = {
     clickCount: 0
   }
@@ -8,16 +12,25 @@ class Clock extends Component {
     return ( 
       <div>
         <h1>you are clicked {this.state.clickCount} time</h1>
-        <button onClick={this.handleClick}>asdasd</button>
+        <button onClick={this.handleClick}>asdf</button>
+        <div onClick={this.handleClick}>
+          <a onClick={this.handleLink} href="https://google.com">google</a>
+        </div>
+        
       </div>
      );
   }
-  handleClick = () => {
+  handleClick(event) {
     this.setState((state) => {
       return {
         clickCount: state.clickCount + 1
       }
     })
+  }
+  handleLink = (event) => {
+    event.preventDefault() // for disable all default event in browser
+    event.stopPropagation() // for disable paent event
+    console.log(event);
   }
 }
 export default Clock;
