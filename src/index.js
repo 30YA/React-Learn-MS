0af25224cd2a5989483453110bb1213b25cf7c9e
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-04-28 15:36:10 +0430

    mini project (Toggle,boxes)

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { createRoot } from 'react-dom/client';
-import App from "./App";
+import Main from "./components/Main";
+import "./components/Style.css"
 
 
 const root = createRoot(document.getElementById("root"));
-root.render(<React.StrictMode><App /></React.StrictMode>);
+root.render(<React.StrictMode><Main /></React.StrictMode>);
