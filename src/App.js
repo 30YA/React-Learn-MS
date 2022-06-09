19616c48ea78dad8bf1358348c33978ec6db4be9
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-06-09 11:22:49 +0430

    creatRef & callBack Ref

--- src/App.js
+++ src/App.js
@@ -1,11 +1,29 @@
-import { Component } from "react";
-import Root from "./components/Root";
-// import 
+import React, { Component } from "react";
+// import
 export default class App extends Component {
-  state = {};
+  constructor(props){
+    super(props);
+    this.textinput = React.createRef(); //with creatRef 1
+    this.setreftodiv = elemnt => { //with callBack Ref 1
+      this.mydiv = elemnt
+    }
+  }
+  sia = () => {
+    this.textinput.current.focus();
+    console.dir(this.textinput.current); //with creatRef 3
+    console.dir(this.mydiv) //with callBack Ref 3
+  }
+  componentDidMount(){
+    // console.dir(document.querySelector('.sia').target);
+  }
+  // state = {};
   render() {
     return (
-      <Root />
+      <>
+        <input type="text" ref={this.textinput} /> {/* with creatRef 2*/}
+        <div ref={this.setreftodiv}>asdasd</div> {/* with callBack Ref 2*/}
+        <input className="sia" type="button" value="focus" onClick={this.sia} />
+      </>
     );
   }
 }
--- src/components/randomColor.js
+++
@@ -1,3 +0,0 @@
-export default function color() {
-    return '#' + Math.floor(Math.random() * 16777215).toString(16);
-}
