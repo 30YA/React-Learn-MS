d2c77616cfe014e5f90a37e7cf539fc324e09b19
siavash aghazadeh <siavash.aghazadeh@gmail.com>
2022-06-13 16:38:25 +0430

    Stale Closure

--- src/App.js
+++ src/App.js
@@ -5,11 +5,64 @@ function App() {
   const [step, setStep] = useState(1);
   useEffect(() => {
     document.title = `counter : ${count}`;
-  })
+    function createIncrement() {
+      let value = 0;
+
+      return function increment() {
+        value = value + 1;
+        console.log(value);
+
+        let message = "";
+        message = `Current value is ${value}`;
+
+        function logger() {
+          console.log(message);
+        }
+
+        function getValue() {
+          return value;
+        }
+
+        return {
+          logger,
+          getValue,
+        };
+      };
+    }
+
+    const counter = createIncrement();
+    // value = 0
+    // func increment
+
+    counter();
+    // value = 1
+    // message1 = 'Current value is 1'
+    // func logger1
+    // func getValue1
+
+    const { logger, getValue } = counter();
+    // value = 2
+    // message2 = 'Current value is 2'
+    // func logger2
+    // func getValue2
+
+    counter();
+    // value = 3
+    // message3 = 'Current value is 3'
+    // func logger3
+    // func getValue3
+
+    logger();
+    console.log(getValue());
+  });
   return (
     <>
       <p>You clicked {count} times!</p>
-      <input value={step} type="number" onChange={(e) => setStep(+e.target.value)}/>
+      <input
+        value={step}
+        type="number"
+        onChange={(e) => setStep(+e.target.value)}
+      />
       <button onClick={() => setCount(count + step)}>Click me!</button>
     </>
   );
